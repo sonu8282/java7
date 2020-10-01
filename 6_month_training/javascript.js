855ddaa81e59ed63fe00b8391efab15db8fa229d
sonu8282 <65779477+sonu8282@users.noreply.github.com>
2020-10-01 13:08:20 +0530

    Add files via upload

---
+++ 6_month_training/javascript.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>remove tags</title>
+</head>
+<body>
+   <ul>
+   	<li>HTML</li>
+   	<li>CSS</li>
+   	<li>JAVASCRIPT</li>
+   	<li>Bootstrap</li>
+   	<li>Python</li>
+   </ul>
+</body>
+</html>
